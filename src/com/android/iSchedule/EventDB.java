c6a129c3e082f36910339525365ecf93ba0b490e
WideLee <limkuan@mail2.sysu.edu.cn>
2013-05-06 11:47:06 +0800

    The database of event

---
+++ src/com/android/iSchedule/EventDB.java
@@ -0,0 +1,40 @@
+package com.android.iSchedule;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class EventDB extends SQLiteOpenHelper {
+
+	private static final String DB_NAME = "event.db";
+	private static final int DB_VRESION = 1;
+	private static final String TABLE_NAME = "event";
+	//private static final String SQL_CREATE_TABLE = "TO DO"
+	
+	
+	public EventDB(Context context, String name, CursorFactory factory,
+			int version) {
+		super(context, name, factory, version);
+		// TODO 自动生成的构造函数存根
+	}
+
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		// TODO 自动生成的方法存根
+		
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		// TODO 自动生成的方法存根
+		
+	}
+	
+	// insert operation
+	// delete operation
+	// update operation
+	// each search operation
+	
+	
+}
