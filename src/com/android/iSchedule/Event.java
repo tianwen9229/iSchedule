36728802778b4171ef3d9d35836cafcfa4356a70
WideLee <limkuan@mail2.sysu.edu.cn>
2013-05-06 11:39:24 +0800

    update

--- src/com/android/iSchedule/Event.java
+++ src/com/android/iSchedule/Event.java
@@ -1,10 +1,25 @@
 package com.android.iSchedule;
 
+import java.sql.Date;
+
 import android.R.integer;
+import android.text.format.Time;
 
 public class Event {
 	
 	private int EventId;
-	private String EventName;
-	private String string;
+	private String EventTitle;
+	private String Place;
+	private int frequency;
+	private String content;
+	
+	private Date createTime;
+	private Date remindTime;
+	private Date startTime;
+	private Date endTime;
+	
+	// To Do 
+	// Setter and Getter
+	
+	
 }
