72072386439be8b07eb86a080587c3135af6da78
WideLee <limkuan@mail2.sysu.edu.cn>
2013-05-12 00:43:34 +0800

    update

--- src/com/android/iSchedule/Event.java
+++ src/com/android/iSchedule/Event.java
@@ -3,6 +3,7 @@ package com.android.iSchedule;
 import java.sql.Date;
 
 import android.R.integer;
+import android.R.string;
 import android.text.format.Time;
 
 public class Event {
@@ -20,6 +21,85 @@ public class Event {
 	
 	// To Do 
 	// Setter and Getter
+	public Event()
+	{}
 	
+	public Event(int id, String title , String place, int fre, String con, Date cT, Date rT, Date sT, Date eT)
+	{
+		EventId = id;
+		EventTitle = title;
+		frequency = fre;
+		content = con;
+		createTime = cT;
+		remindTime = rT;
+		startTime = sT;
+		endTime = eT;
+	}
+	
+	public int getId()
+	{
+		return this.EventId;
+	}
+	public String getTitle() {
+		return this.EventTitle;
+		}
+	public String getPlace() {
+		return this.Place;
+		}
+	public int getFrequency() {
+		return this.frequency;
+		}
+	public String getContent() {
+		return this.content;
+		}
+	public Date getCreatTime() {
+		return this.createTime;
+		}
+	public Date getRemindTime() {
+		return this.remindTime;
+		}
+	public Date getStartTime() {
+		return this.createTime;
+		}
+	public Date getEndTime() {
+		return this.endTime;
+		}
+	
+	public void setId(int id)
+	{
+		this.EventId = id;
+	}
+	
+	public void setTitle(String title)
+	{
+		this.EventTitle = title;
+	}
+	public void setPlace(String place)
+	{
+		this.Place = place;
+	}
+	public void setFrequency(int frequency){
+		this.frequency = frequency;
+	}
+	public void setContent(String content)
+	{
+		this.content = content;
+	}
+	public void setCreatTime(Date cT)
+	{
+		this.createTime = cT;
+	}
+	public void setRemindTime(Date rT)
+	{
+		this.remindTime = rT;
+	}
+	public void setStartTime(Date sT)
+	{
+		this.startTime = sT;
+	}
+	public void setEndTime(Date eT)
+	{
+		this.endTime = eT;
+	}
 	
 }
--- src/com/android/iSchedule/Mode.java
+++ src/com/android/iSchedule/Mode.java
@@ -1,5 +1,7 @@
 package com.android.iSchedule;
 
+import android.R.integer;
+
 public class Mode {
 	
 	private int modeId;
@@ -8,4 +10,38 @@ public class Mode {
 	// test branches
 	// To Do 
 	// Setter and Getter
+	public Mode()
+	{
+	}
+	
+	public Mode(int id, int vo, int vi)
+	{
+	modeId =id;
+	volume = vo;
+	vibrate = vi;
+	}
+	public int getModeId()
+	{
+		return modeId;
+	}
+	public int getVolume()
+	{
+	return volume;
+	}
+	public int getVibrate()
+	{
+		return vibrate;
+	}
+    public void setModeId(int id)
+    {
+    	this.modeId = id;
+    }
+	public void setVolume(int volume)
+	{
+		this.volume = volume;
+	}
+	public void setVibrate(int vibrate)
+	{
+		this.vibrate = vibrate;
+	}
 }
